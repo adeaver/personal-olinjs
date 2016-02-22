1e18bcb98ce3f565a236b24231849d009157000a
Sarah Walters <sarah.walters@students.olin.edu>
2016-02-22 17:05:11 -0500

    Homework 5 feedback

--- restaurants/routes/orders.js
+++ restaurants/routes/orders.js
@@ -14,14 +14,15 @@ var add = function(req, res) {
 
 	var data = [];
 
-	console.log(names);
-	console.log(amounts);
+	/* You could have used a functional tool like Array.prototype.map here -- though it's
+	   a little less convenient because you're looping through both names and amounts.
 
+	   The add function assumes that names and amounts are the same length, yes? Maybe good to
+	   check for that & throw an error (or otherwise handle it) if that's not the case
+	 */
 	for(var index = 0; index < names.length; index++) {
-		var obj = {};
-
-		obj.ingredient = names[index];
-		obj.amount = parseInt(amounts[index]);
+		// can declare an object inline, like this, instead of setting attributes individually
+		var obj = {ingredient: names[index], amount: parseInt(amounts[index])};
 
 		Ingredient.update({"name":obj.ingredient}, {"$inc":{"quantity":-1*obj.amount}}, {}, function(err) {
 			if(err) {
@@ -35,18 +36,33 @@ var add = function(req, res) {
 	}
 
 	var order = new Order({
-		contents:data,
-		price:price
+		contents: data,
+		price: price
 	});
 
+	/* You're adding each obj to data *outside* the Ingredient.update callback, so one of
+		 the updates could fail and you'd still add it to the order and save the order... and you
+		 can't just move the data.push inside the callback, because Ingredient.update is async...
+
+		 This is a pretty good way of dealing with the problem because failing to update the quantity
+		 of an ingredient isn't a) likely or b) that bad.
+
+		 Just so you're aware in the future, though, promises are a way to wait for a bunch of async
+		 operations to finish before doing another operation. "Promise" is a JavaScript built-in, and
+		 there are also a bunch of libraries which work a little differently, depending on what you
+		 need/prefer.
+
+		 In this case, you could make a list of promises associated with each of the Ingredient.update
+		 operations, then create the order and save it after all of those promises have been resolved.
+	 */
 	order.save(function(err, order) {
 		if(err) {
 			errorOccurred = true;
 		}
 	});
 
-	res.send({"message":errorOccurred ? "failed" : "successful"});
-}
+	res.send({message: errorOccurred ? "failed" : "successful"}); // don't need quotes for object keys
+}; // when you declare functions as variables, convention is to end with a semicolon
 
 var remove = function(req, res) {
 	var id = req.query.id;
@@ -60,3 +76,8 @@ var remove = function(req, res) {
 module.exports.home = home;
 module.exports.add = add;
 module.exports.remove = remove;
+/* If you don't like typing out all of the module.exports at the bottom, you can
+	 declare a `var routes = {};` object at the top, then declare each method as
+	 routes.whatever = function() {...};, then at the end `module.exports = routes;`
+	 -- or you can even declare each function directly as `module.exports.whatever = function() {...};
+ */
